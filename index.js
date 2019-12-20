a8385762abcd196ff8eabb48d2dc086312927335
sasha-go <sasha.lfg@gmail.com>
2019-12-19 23:30:10 -0800

    new repo with updated design

---
+++ index.js
@@ -0,0 +1,147 @@
+/* when a user clicks on start quiz button */
+function startQuiz() {
+  $('#startButton').on('click', function(event){
+    renderAQuestion();
+  });
+}
+
+/* Displays question number and score obtained */
+function updateQuestionAndScore() {
+  const html = $(`<ul>
+      <li id="js-answered">Question: ${STORE.currentQuestion + 1} of ${STORE.questions.length}</li>
+      <li id="js-score">Score: ${STORE.score} out of ${STORE.questions.length}</li>
+    </ul>`);
+  $(".question-and-score").html(html);
+}
+
+/* Displays the options for the current question */
+function updateOptions()
+{
+  let question = STORE.questions[STORE.currentQuestion];
+  for(let i=0; i<question.options.length; i++) {
+    $('.js-options').append(`
+      <li class="question-option">
+        <input type = "radio" name="options" id="option${i+1}" value= "${question.options[i]}" tabindex ="${i+1}"> 
+        <label for="option${i+1}"> ${question.options[i]}</label> <br/>
+        <span id="js-r${i+1}"></span>
+      </li>
+    `);
+  }
+  
+}
+
+/*displays the question*/
+function renderAQuestion() {
+  let question = STORE.questions[STORE.currentQuestion];
+  updateQuestionAndScore();
+  const questionHtml = $(`
+  
+  <div>   
+    <form class="quizBox">
+      <fieldset>
+        <div class="row question">
+          <div class="col">
+            <legend> ${question.question}</legend>
+          </div>
+        </div>
+
+        <div class="row options">
+          <div class="col">
+            <ul class="js-options"> </ul>
+        </div>
+      </div>
+    
+
+      <div class="row">
+        <div class="col">
+          <button type = "submit" id="answer" tabindex="5">Submit</button>
+          <button type = "button" id="nextButton" tabindex="6"> Next</button>
+        </div>
+      </div>
+    </fieldset>
+    </form>
+  </div>`);
+$("main").html(questionHtml);
+updateOptions();
+$("#nextButton").hide();
+}
+
+/* displays results and restart quiz button */
+function displayResults() {
+  let resultHtml = $(
+    `<div class="results">
+      <form id="js-restart-quiz">
+        <fieldset>
+          <div class="row">
+            <div class="col">
+              <legend>Your Score is: ${STORE.score}/${STORE.questions.length}</legend>
+              <img id="resultsImg" src="https://www.geeksofdoom.com/GoD/img/2015/09/harry-potter-epilogue-final-shot.jpg" alt="Harry, Ron, Hermione, and Ginny seeing their children head off on the Hogwarts Express"
+            </div>
+          </div>
+        
+          <div class="row">
+            <div class="col">
+              <button type="button" id="restartButton">Retest Your Wits</button>
+            </div>
+          </div>
+        </fieldset>
+    </form>
+    </div>`);
+    STORE.currentQuestion = 0;
+    STORE.score = 0;
+  $("main").html(resultHtml);
+}
+
+/* checks whether it reached the end of questions list */
+function handleQuestions() {
+  $('body').on('click','#nextButton', (event) => {
+    STORE.currentQuestion === STORE.questions.length?displayResults() : renderAQuestion();
+  });
+}
+
+
+/*checks whether the chosen option is right or wrong and displays respective message*/ 
+function handleSelectOption() {
+  $('body').on("submit",'.quizBox', function(event) {
+    event.preventDefault();
+    let currentQues = STORE.questions[STORE.currentQuestion];
+    let selectedOption = $("input[name=options]:checked").val();
+    if (!selectedOption) {
+      alert("Choose an option");
+      return;
+    } 
+    let id_num = currentQues.options.findIndex(i => i === selectedOption);
+    let id = "#js-r" + ++id_num;
+    $('span').removeClass("right-answer wrong-answer");
+    if(selectedOption === currentQues.answer) {
+      STORE.score++; 
+      $(`${id}`).append(`YOU'RE A WIZARD. 100 POINTS FOR GRYFFINDOR!<br/>`);
+      $(`${id}`).addClass("right-answer");
+    }
+    else {
+      $(`${id}`).append(`WRONG!<br/>`);
+      $(`${id}`).addClass("wrong-answer");
+    }
+
+    STORE.currentQuestion++;
+    $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
+    $('#answer').hide();
+    $("input[type=radio]").attr('disabled', true);
+    $('#nextButton').show();
+  });
+}
+
+function restartQuiz() {
+  $('body').on('click','#restartButton', (event) => {
+    renderAQuestion();
+  });
+}
+
+function handleQuizApp() {
+  startQuiz();
+  handleQuestions();
+  handleSelectOption();
+  restartQuiz();
+}
+
+$(handleQuizApp);
---
+++ store.js
@@ -0,0 +1,111 @@
+const STORE = {
+    currentQuestion: 0,
+    score: 0,
+    questions: [
+      {
+        question: 'What auspicious day was Fred and George Weasley born?',
+        options: [
+          'February 3rd, 1978',
+          'April 1st, 1978',
+          'October 23rd, 1977',
+          'October 31st, 1978'],
+        answer: 'April 1st, 1978'
+      },
+  
+      {
+        question: 'In the book Harry Potter and the Goblet of Fire, how did Harry get the idea of using Gillyweed for the second task of the Tri-Wizard Tournament?',
+        options: [
+          'Neville goes to Harry and gives him the idea of using Gillyweed after reading Magical Water Plants of the Mediterranean',
+          'Dobby steals Gillyweed from Professor Snape and gives it Harry after overhearing a conversation between ‘Professor Moody’ and Professor McGonagall about its uses',
+          'Harry reads the Magical Water Plants of the Mediterranean and obtains Gillyweed from Professor Sprout',
+          'Cedric Diggory tips off Harry about Gillyweed after Harry helped him with the first task'],
+          answer: 'Dobby steals Gillyweed from Professor Snape and gives it Harry after overhearing a conversation between ‘Professor Moody’ and Professor McGonagall about its uses'
+      },
+      
+      {
+        question: 'What is the name of the all-female Quidditch team founded in 1203?',
+        options: [
+          'Appleby Arrows',
+          'Holyhead Harpies',
+          'Montrose Magpies',
+          'Bulgarian National Quidditch Team'],
+          answer: 'Holyhead Harpies'
+      },
+      
+      {
+        question: 'What was Nymphadora Tonks’ original patronus before it changed into a wolf?',
+        options: [
+          'Jack Rabbit',
+          'Kangaroo',
+          'Fennec Fox',
+          'Doe'
+        ],
+        answer: 'Jack Rabbit'
+      },
+      
+      {
+        question: 'Both of Hermione’s parents had what occupation in the Muggle World?',
+        options: [
+          'Family Law Lawyers',
+          'Dentists',
+          'Anesthesiologists',
+          'Mail Carriers'
+        ],
+        answer: 'Dentists'
+      },
+      
+      {
+        question: 'Which Harry Potter film(s) did Peeves the Poltergeist make an appearance?',
+        options: [
+          'The Sorcerer’s Stone and The Chamber of Secrets',
+          'The Sorcerer’s Stone',
+          'The Prisoner of Azkaban',
+          'None'
+        ],
+        answer: 'None'
+      },
+      
+      {
+        question: 'Who is the creator of the Sectumsempra spell?',
+        options: [
+          'Tom Riddle',
+          'Severus Snape',
+          'James Potter',
+          'Bellatrix LeStrange'
+        ],
+        answer: 'Severus Snape'
+      },
+      
+      {
+        question: 'Which Weasley sibling never had an acting scene in the Harry Potter films?',
+        options: [
+          'Charlie Weasley',
+          'Molly Weasley',
+          'Bill Weasley',
+          'Percy Weasley'
+        ],
+        answer: 'Charlie Weasley'
+      },
+      
+      {
+        question: 'In the Goblet of Fire, Hermione founded an organization named S.P.E.W in response to the injustices she saw in the treatment of house-elves. What does S.P.E.W stand for?',
+        options: [
+          'Society for Protecting Elves at Work',
+          'Spell Protection for Elves working for Wizards',
+          'Society for the Promotion of Elfish Welfare',
+          'Super Powered Elves at Work'],
+          answer: 'Society for the Promotion of Elfish Welfare'
+      },
+      
+      {
+        question: 'What does Dumbledore says he sees when he looks into the Mirror of Erised?',
+        options: [
+          'He sees himself holding a pair of thick, woolen socks',
+          'He sees himself with his siblings, Ariana and Aberforth',
+          'He sees himself winning the battle against Voldemort',
+          'He sees himself eating a Bertie Bott’s bean that is not booger flavored'
+        ],
+        answer: 'He sees himself holding a pair of thick, woolen socks'
+      }  
+    ]
+  };
